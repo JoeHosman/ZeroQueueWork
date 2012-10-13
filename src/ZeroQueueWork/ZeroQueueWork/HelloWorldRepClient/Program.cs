3ed6461cccf80bdf64af362143dee8e4446d71e3
JoeHosman <joe.hosman@gmail.com>
2012-10-12 21:00:11 -0500

    HelloWorld ReqRep completed
    First example completed... easy peasy

---
+++ src/ZeroQueueWork/ZeroQueueWork/HelloWorldRepClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZMQ;
+
+namespace HelloWorldRepClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context(1))
+            {
+                using (Socket requester = context.Socket(SocketType.REQ))
+                {
+                    requester.Connect("tcp://localhost:5555");
+
+                    const string requestMessage = "Hello";
+                    const int requestsToSend = 10;
+
+                    for (int requestNumber = 0; requestNumber < requestsToSend; requestNumber++)
+                    {
+                        Console.WriteLine("Sending Request {0}...", requestNumber);
+                        requester.Send(requestMessage, Encoding.Unicode);
+
+                        string reply = requester.Recv(Encoding.Unicode);
+                        Console.WriteLine("Received reply {0}: {1}", requestNumber, reply);
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ src/ZeroQueueWork/ZeroQueueWork/HelloWorldRepServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZMQ;
+
+namespace HelloWorldRepServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context(1))
+            {
+                using (Socket receiver = context.Socket(SocketType.REP))
+                {
+                    receiver.Bind("tcp://*:5555");
+
+                    while (true)
+                    {
+                        var message = receiver.Recv(Encoding.Unicode);
+                        Console.WriteLine("Received request: {0}...", message);
+
+                        string reply = "world";
+                        receiver.Send(reply, Encoding.Unicode);
+                    }
+                }
+            }
+        }
+    }
+}
