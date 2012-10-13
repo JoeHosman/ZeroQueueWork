214164f25c81b8effeb0270422b1e61c8f964920
JoeHosman <joe.hosman@gmail.com>
2012-10-12 21:04:07 -0500

    ZeroMQ version reporting
    easy peasy

---
+++ src/ZeroQueueWork/ZeroQueueWork/ZeroQueueVersionCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZMQ;
+
+namespace ZeroQueueVersionCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ZHelpers.Version());
+
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+        }
+    }
+}
