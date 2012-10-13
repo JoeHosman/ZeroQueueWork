c6a62abd96a8c532ac37614428641783fd66a955
JoeHosman <joe.hosman@gmail.com>
2012-10-12 21:34:59 -0500

    Weather Update examples
    completed :D

---
+++ src/ZeroQueueWork/ZeroQueueWork/WeatherUpdateClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZMQ;
+
+namespace WeatherUpdateClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            while (true )
+            {
+
+                int id = rand.Next(1, 100);
+
+                using (var context = new Context(1))
+                {
+                    using (Socket subscriber = context.Socket(SocketType.SUB))
+                    {
+                        subscriber.Subscribe(id.ToString(), Encoding.Unicode);
+                        subscriber.Connect("tcp://localhost:5556");
+
+                        const int updatesToCollect = 100;
+
+                        uint totalTemp = 0;
+
+                        for (int updateNumber = 0; updateNumber < updatesToCollect; updateNumber++)
+                        {
+                            Console.Write(".");
+                            string update = subscriber.Recv(Encoding.Unicode);
+                            totalTemp += (uint)Convert.ToInt32(update.Split()[1]);
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("Avg temp for code [{0}] = {1}", id, totalTemp / updatesToCollect);
+                    }
+                } 
+            }
+
+        }
+    }
+}
---
+++ src/ZeroQueueWork/ZeroQueueWork/WeatherUpdateServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using ZMQ;
+
+namespace WeatherUpdateServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context(1))
+            {
+                using (Socket publisher = context.Socket(SocketType.PUB))
+                {
+                    publisher.Bind("tcp://*:5556");
+
+                    var rand = new Random();
+
+                    while (true)
+                    {
+                        var id = rand.Next(0, 100).ToString();
+                        var message = rand.Next(0, 100).ToString("000");
+
+                        string format = string.Format("{0} {1}", id, message);
+                        publisher.Send(format, Encoding.Unicode);
+                        Console.WriteLine("Temp sent: {0}...", format);
+                        //Thread.Sleep(new TimeSpan(0, 0, 30));
+                    }
+                }
+            }
+        }
+    }
+}
